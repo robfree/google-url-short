bb58d46b26bb59da925b3e7990bd383b93f9f914
robfree <robertoserrano83@gmail.com>
2015-07-03 16:52:03 +0200

    Modulo google-ulr-short

---
+++ google-url-short.js
@@ -0,0 +1,62 @@
+/**
+ * Modulo que contiene clases para el uso de Google API Url short
+ * La refencia de API se puede consultar en:
+ *    https://developers.google.com/url-shortener/v1/?hl=es
+ *
+ * @module googleShort
+ * @author Roberto Serrano Diaz-Grande
+ *
+ */
+var googleShort = googleShort || {};
+
+/**
+ * 'API_KEY_GOOGLE' constante con la API Key de Google
+ * @property API_KEY_GOOGLE
+ * @type  String
+ * @static
+ * @final
+ *
+ */
+googleShort.API_KEY_GOOGLE = 'AIzaSyD-hiA4JGBYXVkxmhL0pAi9fMpWlk5n-z8';
+
+/**
+ * Retorna el la url lista para obtener información de un enlace corto
+ * @method get
+ * @static
+ * @throws {Error} Nuevo Error
+ * @example
+ *    googleShort.get('shortUrl', 'projection');
+ * @since 0.5.0
+ */
+googleShort.get = function(shortUrl, projection) {
+  return 'https://www.googleapis.com/urlshortener/v1/url?key=' + googleShort.API_KEY_GOOGLE + '&shortUrl=' + shortUrl + "&projection=" + projection;
+}
+
+/**
+ * Retorna un objeto con las propiedades: parametros a enviar al servidor,
+ *  y el enlace para la petición
+ * @class insert
+ * @constructor
+ * @param shortUrl {String} El enlace que se desea cortar
+ *
+ */
+googleShort.insert = function (longUrl) {
+
+  /**
+   * Continene los parametros para enviar
+   * @property parametros
+   * @type JSON
+   */
+  var parametros = JSON.stringify({ longUrl: longUrl }, null, '\t');
+  /**
+   * Contiene el enlace listo para la peticion
+   * @property enlace
+   * @type String
+   */
+  var enlace = 'https://www.googleapis.com/urlshortener/v1/url?key=' + googleShort.API_KEY_GOOGLE;
+
+  return {
+      parametros: parametros,
+      enlace: enlace
+  };
+}
